b5e8f8e9380d4f14640c5d451e9eb265f139780b
ahale <a.hale@live.ca>
2019-07-02 13:57:00 -0400

    potential fix for fopen error message

--- app/saveFlavourHistory.php
+++ app/saveFlavourHistory.php
@@ -1,7 +1,7 @@
 <?php
     $data = $_POST['data'];
     $fname = "flavourBackup-" . date("Y-m-d") . ".json";
-    $file = fopen("tubs/" . $fname, 'w');
+    $file = fopen("tubs/" . $fname, "w");
     fwrite($file, $data);
     fclose($file);
 ?>
