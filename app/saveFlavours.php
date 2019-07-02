c3f1ea468b74e2c661bec74dcb936a5f0e4cff88
ahale <a.hale@live.ca>
2019-07-02 14:07:20 -0400

    change PHP filepaths

--- app/saveFlavours.php
+++ app/saveFlavours.php
@@ -1,7 +1,7 @@
 <?php
     $data = $_POST['data'];
     $fname = "flavours.json";
-    $file = fopen("tubs/" . $fname, "w");
+    $file = fopen("../tubs/" . $fname, "w");
     fwrite($file, $data);
     fclose($file);
 ?>
