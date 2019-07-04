a92a6d7a1394bb89ebcd64cb8b72a207115b1e45
ahale <a.hale@live.ca>
2019-07-03 23:17:59 -0400

    prettify JSON file saving

--- app/saveFlavours.php
+++ app/saveFlavours.php
@@ -2,6 +2,6 @@
     $data = json_decode($_POST["data"]);
     $fname = "flavours.json";
     $file = fopen("../tubs/" . $fname, "w");
-    fwrite($file, json_encode($data));
+    fwrite($file, json_encode($data, JSON_PRETTY_PRINT));
     fclose($file);
 ?>
