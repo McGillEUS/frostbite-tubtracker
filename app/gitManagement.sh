427e4b81653f9b468ed92f3b4bcd3f51859936f2
ahale <a.hale@live.ca>
2019-08-10 00:00:26 -0400

    add chown and app//* to gitManagement script

--- app/gitManagement.sh
+++ app/gitManagement.sh
@@ -4,3 +4,6 @@ git commit -m "flavour history update";
 git pull;
 git push;
 chmod 777 /srv/www/frostbite/tubtracker/tubs/*;
+chown frostbite:frostbite /srv/www/frostbite/tubtracker/tubs/*;
+chmod 775 /srv/www/frostbite/tubtracker/app/*;
+chown frostbite:frostbite /srv/www/frostbite/tubtracker/app/*;
